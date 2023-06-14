53cf4ea4d797c084cc2813cc4853769e75d4d897
Saif Ali <sterdale@mobiquityinc.com>
2023-06-14 20:21:52 +0530

    Project and Pod Setup

---
+++ Imgur-Gallery/Pods/Target Support Files/Pods-Imgur-Gallery-Imgur-GalleryUITests/Pods-Imgur-Gallery-Imgur-GalleryUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Imgur_Gallery_Imgur_GalleryUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Imgur_Gallery_Imgur_GalleryUITests
+@end
---
+++ Imgur-Gallery/Pods/Target Support Files/Pods-Imgur-GalleryTests/Pods-Imgur-GalleryTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Imgur_GalleryTests : NSObject
+@end
+@implementation PodsDummy_Pods_Imgur_GalleryTests
+@end
